d32ee404d26e6623c80ceadcf66ac7d8b532b16b
Heneli Kailahi <heneli@kailahi.com>
2017-07-31 21:43:27 -0700

    Added in place rotation, and rotation about an inputted length.

--- src/BitAlgos.hs
+++ src/BitAlgos.hs
@@ -23,46 +23,90 @@ reverseBits n = fromBinString $ reverse $ toBin n
 oddParity :: Int -> Bool -- Checks if there is an odd number of 1-bits
 oddParity n = ((mod (countSetBits n) 2) /= 0)
 
--- NOTE: All rotation methods are hardcoded for 12 bit rotation
+rotateL_12Bit :: Int -> Int -> Int
+rotateL_12Bit n d = let rotations = mod d 12
+                        bitsize = 12
+                        bitMask = fromBinDigits $ take bitsize [1,1..]
+                        -- xxx00000 get first (12 - rotations) bits
+                        front   = shiftL (shiftR bitMask (rotations) .&. n) rotations
+                        -- 000xxxxx get rest (rotations) bits
+                        back    = shiftR n (bitsize - rotations)
+                    in front .|. back
 
-rotateL12 :: Int -> Int -> Int
-rotateL12 n d = let rotations = mod d 12
-                    bitsize = 12
-                    bitMask = fromBinDigits $ take bitsize [1,1..]
-                    -- xxx00000 get first (12 - rotations) bits
-                    front   = shiftL (shiftR bitMask (rotations) .&. n) rotations
-                    -- 000xxxxx get rest (rotations) bits
-                    back    = shiftR n (bitsize - rotations)
-                in front .|. back
+rotateR_12Bit :: Int -> Int -> Int
+rotateR_12Bit n d = let rotations = mod d 12
+                        bitsize = 12
+                        bitMask = fromBinDigits $ take bitsize [1,1..]
+                        -- xxx00000 get first rotations bits
+                        front   = shiftL ((shiftR bitMask (bitsize - rotations)) .&. n) (bitsize - rotations)
+                        -- 000xxxxx get rest (12 - rotations) bits
+                        back    = shiftR n rotations
+                    in front .|. back
 
-rotateR12 :: Int -> Int -> Int
-rotateR12 n d = let rotations = mod d 12
-                    bitsize = 12
-                    bitMask = fromBinDigits $ take bitsize [1,1..]
-                    -- xxx00000 get first rotations bits
-                    front   = shiftL ((shiftR bitMask (bitsize - rotations)) .&. n) (bitsize - rotations)
-                    -- 000xxxxx get rest (12 - rotations) bits
-                    back    = shiftR n rotations
-                in front .|. back
+rotateL_12Bit' :: Int -> Int -> Int
+rotateL_12Bit' n d = let bitsize     = 12
+                         rotations   = mod d bitsize
+                         bMinusR     = bitsize - rotations
+                         shiftedMask = fromBinDigits $ take (bMinusR) [1,1..]
+                         -- xxx00000 get first (12 - rotations) bits
+                         front       = shiftL (shiftedMask .&. n) rotations
+                         -- 000xxxxx get rest (rotations) bits
+                         back        = shiftR n bMinusR
+                      in front .|. back
 
-rotateL12' :: Int -> Int -> Int
-rotateL12' n d = let bitsize     = 12
-                     rotations   = mod d bitsize
-                     bMinusR     = bitsize - rotations
-                     shiftedMask = fromBinDigits $ take (bMinusR) [1,1..]
-                     -- xxx00000 get first (12 - rotations) bits
-                     front       = shiftL (shiftedMask .&. n) rotations
-                     -- 000xxxxx get rest (rotations) bits
-                     back        = shiftR n bMinusR
-                 in front .|. back
+rotateR_12Bit' :: Int -> Int -> Int
+rotateR_12Bit' n d = let bitsize       = 12
+                         rotations     = mod d bitsize
+                         bMinusR       = bitsize - rotations
+                         unshiftedMask = fromBinDigits $ take (rotations) [1,1..]
+                         -- xxx00000 get first (12 - rotations) bits
+                         front         = shiftL (unshiftedMask .&. n) bMinusR
+                         -- 000xxxxx get rest (rotations) bits
+                         back          = shiftR n (rotations)
+                     in front .|. back
 
-rotateR12' :: Int -> Int -> Int
-rotateR12' n d = let bitsize       = 12
-                     rotations     = mod d bitsize
-                     bMinusR       = bitsize - rotations
-                     unshiftedMask = fromBinDigits $ take (rotations) [1,1..]
-                     -- xxx00000 get first (12 - rotations) bits
-                     front         = shiftL (unshiftedMask .&. n) bMinusR
-                     -- 000xxxxx get rest (rotations) bits
-                     back          = shiftR n (rotations)
-                in front .|. back
+
+
+rotateL_InPlace :: Int -> Int -> Int
+rotateL_InPlace n d = let bitsize     = binLength n
+                          rotations   = mod d bitsize
+                          bMinusR     = bitsize - rotations
+                          shiftedMask = fromBinDigits $ take (bMinusR) [1,1..]
+                          -- xxx00000 get first (12 - rotations) bits
+                          front       = shiftL (shiftedMask .&. n) rotations
+                          -- 000xxxxx get rest (rotations) bits
+                          back        = shiftR n bMinusR
+                      in front .|. back
+
+rotateR_InPlace :: Int -> Int -> Int
+rotateR_InPlace n d = let bitsize       = binLength n
+                          rotations     = mod d bitsize
+                          bMinusR       = bitsize - rotations
+                          unshiftedMask = fromBinDigits $ take (rotations) [1,1..]
+                          -- xxx00000 get first (12 - rotations) bits
+                          front         = shiftL (unshiftedMask .&. n) bMinusR
+                          -- 000xxxxx get rest (rotations) bits
+                          back          = shiftR n (rotations)
+                      in front .|. back
+
+rotateL_nBits :: Int -> Int -> Int -> Int
+rotateL_nBits n d bs = let bitsize     = bs
+                           rotations   = mod d bitsize
+                           bMinusR     = bitsize - rotations
+                           shiftedMask = fromBinDigits $ take (bMinusR) [1,1..]
+                           -- xxx00000 get first (12 - rotations) bits
+                           front       = shiftL (shiftedMask .&. n) rotations
+                           -- 000xxxxx get rest (rotations) bits
+                           back        = shiftR n bMinusR
+                       in front .|. back
+
+rotateR_nBits :: Int -> Int -> Int -> Int
+rotateR_nBits n d bs = let bitsize       = bs
+                           rotations     = mod d bitsize
+                           bMinusR       = bitsize - rotations
+                           unshiftedMask = fromBinDigits $ take (rotations) [1,1..]
+                           -- xxx00000 get first (12 - rotations) bits
+                           front         = shiftL (unshiftedMask .&. n) bMinusR
+                           -- 000xxxxx get rest (rotations) bits
+                           back          = shiftR n (rotations)
+                       in front .|. back
--- src/Welcome.hs
+++ src/Welcome.hs
@@ -7,12 +7,12 @@ import Text.Printf
 
 allPrintMethods :: String
 allPrintMethods =  "- printBinAnd :: Int -> Int -> IO ()" ++ "\n"
-                     ++ "- printBinOr :: Int -> Int -> IO ()" ++ "\n"
-                     ++ "- printBinXOR :: Int -> Int -> IO ()" ++ "\n"
-                     ++ "- printBinShiftR :: Int -> Int -> IO ()" ++ "\n"
-                     ++ "- printBinShiftL :: Int -> Int -> IO ()" ++ "\n"
-                     ++ "- printBinAdd :: Int -> Int -> IO ()" ++ "\n"
-                     ++ "- printBinMinus :: Int -> Int -> IO ()" ++ "\n"
+                ++ "- printBinOr :: Int -> Int -> IO ()" ++ "\n"
+                ++ "- printBinXOR :: Int -> Int -> IO ()" ++ "\n"
+                ++ "- printBinShiftR :: Int -> Int -> IO ()" ++ "\n"
+                ++ "- printBinShiftL :: Int -> Int -> IO ()" ++ "\n"
+                ++ "- printBinAdd :: Int -> Int -> IO ()" ++ "\n"
+                ++ "- printBinMinus :: Int -> Int -> IO ()" ++ "\n"
 
 allBitOps :: String
 allBitOps =  "** type Digits = [Int] **" ++ "\n"
@@ -25,12 +25,18 @@ allBitOps =  "** type Digits = [Int] **" ++ "\n"
           ++ "- stringOfBinDigits :: Int -> String" ++ "\n"
           ++ "- fromDigits :: Digits -> Int" ++ "\n"
           ++ "- fromBinDigits :: Digits -> Int" ++ "\n"
+          ++ "- fromBinString :: String -> Int" ++ "\n"
           ++ "- matchZeroes :: Int -> Int -> Digits" ++ "\n"
-          ++ "- countSetBits :: Int -> Int" ++ "\n"
-          ++ "- rotateR' :: Int -> Int -> Int" ++ "\n"
-          ++ "- rotateL' :: Int -> Int -> Int" ++ "\n"
-          ++ "- rotateL12 :: Int -> Int -> Int" ++ "\n"
-          ++ "- rotateR12 :: Int -> Int -> Int" ++ "\n"
+
+allBitAlgos :: String
+allBitAlgos = "- countSetBits :: Int -> Int" ++ "\n"
+            ++ "- hammingDistance :: Int -> Int -> Int" ++ "\n"
+            ++ "- reverseBits :: Int -> Int" ++ "\n"
+            ++ "- oddParity :: Int -> Bool" ++ "\n"
+            ++ "- rotateL12 :: Int -> Int -> Int" ++ "\n"
+            ++ "- rotateR12 :: Int -> Int -> Int" ++ "\n"
+            ++ "- rotateL12' :: Int -> Int -> Int" ++ "\n"
+            ++ "- rotateR12' :: Int -> Int -> Int" ++ "\n"
 
 main = do
-  putStrLn $ printf "%s\n\n %s\n%s\n\n %s\n%s" "Welcome to the BitVisual!" " Try a bitwise operations:" allPrintMethods "Or compose your own function with these:" allBitOps
+  putStrLn $ printf "%s\n\n %s\n%s\n\n %s\n%s\n\n %s\n%s" "Welcome to the BitVisuals!" " Try printing some basic bitwise operations:" allPrintMethods "Or compose your own function with these:" allBitOps "We've made useful algorithms for you too:" allBitAlgos
