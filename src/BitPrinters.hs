2e78e2ea400f872a347db01351fa90c406dff332
Heneli Kailahi <heneli@kailahi.com>
2018-07-10 16:51:23 -0400

    Clean up part 3 - Moved helpers to internal module. Next episode: The nightmare that is printf, or differentiating between Binary and Decimal digit types?

--- src/BitPrinters.hs
+++ src/BitPrinters.hs
@@ -9,39 +9,8 @@ module BitPrinters
 import Data.Bits ((.|.), (.&.), shiftL, shiftR, xor)
 import Text.Printf (printf)
 
-import BitOps (digitsToString, matchZeroes, toBinString)
-
--- ************************** PRINTING HELPER METHODS ************************** --
-
-printBinOp' :: Word -> Word -> Word -> [String]
-printBinOp' a b result = let leadingZero x y = digitsToString $ matchZeroes x y
-                             a' = leadingZero b a
-                             b' = leadingZero a b
-                             r'
-                                | a > b     = leadingZero a result
-                                | otherwise = leadingZero b result
-                         in blend (fmap (show) [a,b,result]) [a', b', r']
-
-blend :: [a] -> [a] -> [a]
-blend (x:xs) ys = x:(blend ys xs)
-blend _ _ = []
-
-printfFromParamList6 :: String -> [String] -> IO ()
-printfFromParamList6 op params = let nAndB = "\t%+5s %+14s\n"
-                                     printable = "\tDecimal      Binary\n"
-                                                 ++ nAndB
-                                                 ++ op
-                                                 ++ nAndB
-                                                 ++ "------------------------------\n"
-                                                 ++ nAndB
-                                 in putStrLn $ printf printable ((!!) params 0)
-                                                                ((!!) params 1)
-                                                                ((!!) params 2)
-                                                                ((!!) params 3)
-                                                                ((!!) params 4)
-                                                                ((!!) params 5)
-
--- ************************** BITWISE OPERATION PRINTING METHODS ************************** --
+import BitOps (toBinString)
+import Internal.BitPrinters
 
 printBinAdd :: Word -> Word -> IO ()
 printBinAdd a b = let result = (+) a b
